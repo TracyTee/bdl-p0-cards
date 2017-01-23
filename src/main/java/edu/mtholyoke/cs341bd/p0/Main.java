8d3244c0eeed6ef3030e22ad8870197abdcdf0e4
John Foley <jfoley@cs.umass.edu>
2017-01-23 12:29:19 -0500

    Starting point, code. Example of JUnit tested CLI component

---
+++ src/main/java/edu/mtholyoke/cs341bd/p0/Main.java
@@ -0,0 +1,10 @@
+package edu.mtholyoke.cs341bd.p0;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programming Assignment #0: Rummy Scoring");
+    System.out.println("CS341 - Building a Digital Library");
+    // TODO adjust your author name.
+    System.out.println("Author: John Foley");
+  }
+}
